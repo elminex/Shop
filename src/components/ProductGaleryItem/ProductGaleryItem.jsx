531c7acb7f8e5d89712b843d4131b4e765a831d6
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-08-29 20:17:01 +0200

    Finish basic template of main page

--- src/components/ProductGaleryItem/ProductGaleryItem.jsx
+++ src/components/ProductGaleryItem/ProductGaleryItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductGaleryItem = ({ elem }) => (
-  <div style={{ backgroundImage: `url(${elem.photo})` }}>
-    <span>{elem.name}</span>
-    <span>{elem.price}</span>
-  </div>
+  <Link to={`/product/${elem.id}`} className="main-page__product" style={{ backgroundImage: `url(${elem.photo})` }}>
+    <div className="main-page__product-overlay">
+      <div className="main-page__product-line" />
+      <p className="main-page__product-price">${elem.price}</p>
+      <h2 className="main-page__product-name">{elem.name}</h2>
+    </div>
+  </Link>
 );
 
 export default ProductGaleryItem;
